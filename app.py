78cacec2f8732922868cf1b8245a2fc4d913139d
Edet Joseph <144073368+joedetDS@users.noreply.github.com>
2024-12-18 22:04:55 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler  # Importing scaler
 
 # Load the Random Forest model
-with open('Random_Forest_model.pkl', 'rb') as file:
+with open('random_forest_model.pkl', 'rb') as file:
     rf_model = pickle.load(file)
 
 # Load the scaler (ensure the same scaler used in training)
@@ -19,28 +19,22 @@ Provide the values for the following parameters:
 """)
 
 # Arrange input fields side by side
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-    palmitic_acid = st.slider("Palmitic Acid (%)", min_value=23.79, max_value=59.63, value=40.03, step=0.01)
-    iodine_value = st.slider("Iodine Value (g/100g)", min_value=50.00, max_value=69.99, value=59.98, step=0.01)
-    density = st.slider("Density (g/cm³)", min_value=0.8007, max_value=0.9646, value=0.8898, step=0.0001, format="%.4f")
-
-with col2:
     oleic_acid = st.slider("Oleic Acid (%)", min_value=13.62, max_value=73.23, value=44.92, step=0.01)
-    saponification_value = st.slider("Saponification Value (mg KOH/g)", min_value=190.00, max_value=209.99, value=199.89, step=0.01)
-    viscosity = st.slider("Viscosity (cP)", min_value=30.01, max_value=99.99, value=64.72, step=0.01)
-
-with col3:
     peroxide_value = st.slider("Peroxide Value (meq/kg)", min_value=1.00, max_value=14.99, value=8.04, step=0.01)
     free_fatty_acid = st.slider("Free Fatty Acid (%)", min_value=0.10, max_value=5.00, value=2.61, step=0.01)
-    color_index = st.slider("Color Index (Lovibond units)", min_value=1.00, max_value=12.00, value=6.48, step=0.01)
+
+with col2:
+    iodine_value = st.slider("Iodine Value (g/100g)", min_value=50.00, max_value=69.99, value=59.98, step=0.01)
+    saponification_value = st.slider("Saponification Value (mg KOH/g)", min_value=190.00, max_value=209.99, value=199.89, step=0.01)
+    viscosity = st.slider("Viscosity (cP)", min_value=30.01, max_value=99.99, value=64.72, step=0.01)
 
 # Predict button
 if st.button("Predict"):
     # Create a feature array
-    features = np.array([[palmitic_acid, oleic_acid, peroxide_value, free_fatty_acid,
-                          iodine_value, saponification_value, color_index, density, viscosity]])
+    features = np.array([[oleic_acid, peroxide_value, free_fatty_acid, iodine_value, saponification_value, viscosity]])
 
     # Apply scaling to the input features
     features_scaled = scaler.transform(features)
